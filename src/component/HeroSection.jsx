82d55a58926d294828b14617f806d6249c2557fe
Sathish Pooja <Sathish Pooja@DESKTOP-S39ETOQ>
2025-01-04 19:26:23 +0530

    Test the Mobil

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -4,11 +4,11 @@ const HeroSection = () => {
   return (
     <div className='flex mobile:flex-col mobile:h-screen mobile:justify-center'>
 
-      <div className='flex-1 mobile:flex mobile:items-center mobile:justify-center'  style={{backgroundColor:'red'}}>
+      <div className='flex-2 mobile:flex mobile:items-center mobile:justify-center'  style={{backgroundColor:'red'}}>
         <h1 className='mobile:text-7xl text-center'>Dronas</h1>
       </div>
 
-      <div className='flex-3' style={{backgroundColor:'skyblue'}}>
+      <div className='flex-1' style={{backgroundColor:'skyblue'}}>
         <HeroModel />
       </div>
 
